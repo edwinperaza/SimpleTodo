285e0bc054a99fda04768cb382ede28ed8177272
Edwin Peraza <edwinperaza87@gmail.com>
2015-07-04 16:35:00 -0430

    Improve listview item

--- app/src/main/java/com/example/edwinmperazaduran/simpletodo/TodoItemAdapter.java
+++ app/src/main/java/com/example/edwinmperazaduran/simpletodo/TodoItemAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -17,6 +18,8 @@ public class TodoItemAdapter extends ArrayAdapter<TodoItem> {
 
     private Context context;
     private static DateFormat df = DateFormat.getDateInstance();
+    private SimpleDateFormat month = new SimpleDateFormat("MMMM") ;
+    private SimpleDateFormat day = new SimpleDateFormat("d");
 
     public TodoItemAdapter(Context context, ArrayList<TodoItem> items) {
         super(context,0, items);
@@ -29,14 +32,15 @@ public class TodoItemAdapter extends ArrayAdapter<TodoItem> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,null);
         }
         TextView itemView = (TextView) convertView.findViewById(R.id.tvTodoItem);
-        TextView dateView = (TextView) convertView.findViewById(R.id.tvDueDate);
+        TextView dateMonthView = (TextView) convertView.findViewById(R.id.tvDueMonth);
+        TextView dateDayView = (TextView) convertView.findViewById(R.id.tvDueDay);
         TextView priorityView = (TextView) convertView.findViewById(R.id.tvPriority);
 
         if (item != null) {
-                itemView.setText(item.getName());
-                String dateStr = df.format(item.getDueDate());
-                dateView.setText(dateStr);
-                priorityView.setText(item.getPriority());
+            itemView.setText(item.getName());
+            dateMonthView.setText(month.format(item.getDueDate()));
+            priorityView.setText(item.getPriority());
+            dateDayView.setText(day.format(item.getDueDate()));
         }
         return convertView;
     }
